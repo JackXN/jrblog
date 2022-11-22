fb22f05513f924536293aa5bad41cd1c09400cc7
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-11-21 19:24:42 -0700

    Icons + Sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css';
 
 // Components
 import Nav from '../src/components/Containers/Navbar/Nav';
+import {Sidebar as Example} from '../src/sidebars/sidebar-2/Sidebar';
 
 
 export default function Home() {
@@ -17,14 +18,16 @@ export default function Home() {
         <meta itemProp='description' content='Welcome To Nodus. A Technical Blog built for software developers and and techies.'/>
         <meta name='description' content='Welcome To Nodus. A Technical Blog built for software developers and and techies.'/>
         <meta property='og:title' content='The Nodus Blog'/>
-        <meta property='og:url' content/>
-
+        <meta property='og:url'/>
         <link rel="icon" href="/favicon.ico" />
-
+        <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,600,0,0"
+    />
       </Head>
 
-<Nav/>
-    
+{/* <Nav/> */}
+  <Example/>
     </div>
   )
 }
