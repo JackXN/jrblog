7f75683a44fb6e484c622d2579a5d878d348d39a
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-11-24 09:55:13 -0700

    cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,25 +20,3 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
 
-
-// //! Updated Attempt. 
-
-
-// import '../styles/globals.css';
-// import type { AppProps } from 'next/app';
-// import React from 'react';
-// import {BrowserRouter as Router} from 'react-router-dom';
-// // import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
-// import Auth0ProviderWithHistory from '../src/auth/auth0-provider-with-history';
-
-
-// export default function App({Component, pageProps} : AppProps){
-//   return (
-//     <Router>
-//       <Auth0ProviderWithHistory>
-//         <Component {...pageProps}/>
-//       </Auth0ProviderWithHistory>
-//     </Router>
-//   )
-// }
-
