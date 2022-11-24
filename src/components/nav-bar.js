4097640d22a81d77c10aec8339536ee96eac2dc0
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-11-24 10:26:36 -0700

    might be some breaking changes

---
+++ src/components/nav-bar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import MainNav from './main-nav';
+import AuthNav from './auth-nav';
+
+const Navbar = () => {
+return (
+<div className='nav-container mb-3'> 
+<nav className='navbar navbar-expand-md navbar-light bg-light'>
+
+<div className='container'>
+<div className='navbar-brand logo'>
+<MainNav/>
+<AuthNav/>
+</div>
+</div>
+
+</nav>
+
+</div>
+)
+}
+
+
+export default Navbar;
+
